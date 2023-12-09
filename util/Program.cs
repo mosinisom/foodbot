8318a7c984ec7abc93a5c165ebee1fd92b423741
alex mosin <alexmosin@inbox.ru>
2023-12-09 22:55:16 +0400

    start

---
+++ util/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using OfficeOpenXml;
+
+void ProcessFile()
+{
+    var lines = File.ReadAllLines("list.txt");
+    using (var package = new ExcelPackage(new FileInfo("output.xlsx")))
+    {
+        var worksheet = package.Workbook.Worksheets.Add("Sheet1");
+        for (int i = 0; i < lines.Length; i++)
+        {
+            var parts = lines[i].Split(" - ");
+            if (parts.Length == 2)
+            {
+                worksheet.Cells[i + 1, 1].Value = parts[0].Trim();
+                worksheet.Cells[i + 1, 2].Value = parts[1].Trim().TrimEnd('.');
+            }
+        }
+        package.Save();
+    }
+}
+
+ProcessFile();
